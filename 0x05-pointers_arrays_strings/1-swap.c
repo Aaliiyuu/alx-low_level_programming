3d05ffb908f0444668bc9d8c505138ef84e3ed47
Aaliiyuu <aliumukhtar123@gmail.com>
2023-08-22 21:05:39 +0100

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - @ab: An integer to swap
+ * @b: Another integer to swap
+ * @a: integer to swap
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int aux;
+
+aux = *a;
+*a = *b;
+*b = aux;
+}
